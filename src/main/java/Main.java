e14aae34efb8677242405d3db93e3b136816dbc3
AndrewsSkBaron <baron_11_2012@mail.ru>
2022-02-22 16:08:14 +0200

    Commit init #6 task_60_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,16 +1,4 @@
-import org.openqa.selenium.WebDriver;
-import org.selenim.driver.Driver;
-import org.selenim.pages.LogInPage;
-import org.selenim.pages.ProfilePage;
-
 public class Main {
     public static void main(String[] args) {
-        WebDriver driver = Driver.getInstance();
-        driver.get("https://mail.yandex.com/");
-        LogInPage logInPage = new LogInPage(driver);
-        logInPage.loginWithData("LoginTestDriver1", "test0102");
-        ProfilePage profilePage = new ProfilePage(driver);
-        profilePage.actionLogOut();
-        Driver.quit();
     }
 }
