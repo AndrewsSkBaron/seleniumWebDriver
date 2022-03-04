8ea389ea47717d2ca3fb068d69186b56392da1e6
AndrewsSkBaron <baron_11_2012@mail.ru>
2022-03-04 11:13:11 +0200

    Commit init #3 task_110

--- src/main/java/org/selenim/driver/Driver.java
+++ src/main/java/org/selenim/driver/Driver.java
@@ -47,25 +47,4 @@ public class Driver {
         webDriver.quit();
         webDriver = null;
     }
-
-//    private static WebDriver webDriver;
-//    private static String path = "src/main/resources/drivers/geckodriver/geckodriver.exe";
-//    private static String gecko = "webdriver.gecko.driver";
-//
-//    private Driver() {
-//    }
-//    public static WebDriver getInstance() {
-//        if (webDriver == null) {
-//            System.setProperty(gecko,path);
-//            webDriver = new FirefoxDriver();
-//            Objects.requireNonNull(webDriver).manage().window().maximize();
-//            webDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-//        }
-//        return webDriver;
-//    }
-//
-//    public static void quit() {
-//        webDriver.quit();
-//        webDriver = null;
-//    }
 }
